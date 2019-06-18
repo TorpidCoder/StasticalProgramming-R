06ed8cc34ee8c5d5f471d7881779307383233fae
Sahil Nagpal <sahilexemplary@gmail.com>
2019-06-18 22:35:29 +0530

    Create Math-Functions.R

---
+++ Advanced-R-Programming/Math-Functions.R
@@ -0,0 +1,17 @@
+#print the absolute values
+
+print(abs(-2))
+
+#print the sum of values
+
+v <- c(1,2,3,4,5)
+
+print(sum(v))
+
+#print mean of a series
+
+print(mean(v))
+
+#print the round of
+num <- 2.3333345
+print(round(num,digits = 2))
