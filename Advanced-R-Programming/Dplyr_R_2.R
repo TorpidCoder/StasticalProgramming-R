8e9a27088fdc3bd4efaa1c339869ace904596c6d
Sahil Nagpal <sahilexemplary@gmail.com>
2019-06-23 10:06:56 +0530

    Update Dplyr_R_2.R

--- Advanced-R-Programming/Dplyr_R_2.R
+++ Advanced-R-Programming/Dplyr_R_2.R
@@ -27,4 +27,17 @@ head(mutate(flights,actualtime = dep_time-dep_delay))
 # to get the only new column
 head(transmute(flights,actualtime = dep_time-dep_delay))
 
+
+#to get the mean or to perform any aggregation
+
+summarise(flights,avg_air_time = mean(air_time,na.rm = TRUE))
+
+summarise(flights,total_air_time = sum(air_time,na.rm = TRUE))
+
+#to create random rows or sample data 
+
+sample_n(flights,10)
+
+# to create a random rows by percentage 
+sample_frac(flights,0,1)
          
